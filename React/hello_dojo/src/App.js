726075cdbda51504c61d208014902cdc49d6bf34
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-08-30 15:11:02 -0700

    hello dojo

---
+++ React/hello_dojo/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello Dojo</h1>
+            </header>
+            <section>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default App;

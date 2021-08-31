0c5c943fa7a52fbd414f4edc98b66f970f268ae9
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-08-31 08:58:15 -0700

    start

---
+++ React/todolist/src/components/ToDoList.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+const ToDoList = (props) => {
+    const arrayOfLists = [
+        {
+            title: "List One",
+            tasks: {"eat": true, 
+                "sleep": true,
+                "pay taxes": true
+                }
+        }
+    ]
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default ToDoList;

c87d7b9d06be96cb2a9f38379299c70c2b9776fb
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-09-06 13:32:36 -0700

    bug w/ npm install @reach/router

---
+++ Full-Stack-MERN/first/client/public/Main.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+import ProductForm from './components/ProductForm';
+import ProductList from './components/ProductList';
+import axios from 'axios';
+export default () => {
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/products')
+            .then(res=>{
+                setProducts(res.data);
+                setLoaded(true);
+            });
+    },[])
+    return (
+        <div>
+            <ProductForm />
+            {loaded && <ProductList products={products}/>}
+        </div>
+    )
+}
